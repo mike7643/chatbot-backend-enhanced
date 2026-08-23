c45dcb8c4b8a7ad304c0e9aceca6204aed1487fa
mike7643 <developerJYumin@gmail.com>
2026-08-23 17:15:24 +0900

    feat: 추천 컨텍스트 Redis 세션 저장 구현

--- src/main/java/com/comprehensive/eureka/chatbot/langchain/dto/RecommendPlanDto.java
+++ src/main/java/com/comprehensive/eureka/chatbot/langchain/dto/RecommendPlanDto.java
@@ -1,11 +1,15 @@
 package com.comprehensive.eureka.chatbot.langchain.dto;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecommendPlanDto {
     private PlanDto plan;
     private Double score;
--- src/main/java/com/comprehensive/eureka/chatbot/langchain/dto/UserPreferenceDto.java
+++ src/main/java/com/comprehensive/eureka/chatbot/langchain/dto/UserPreferenceDto.java
@@ -1,10 +1,14 @@
 package com.comprehensive.eureka.chatbot.langchain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserPreferenceDto {
 
     private Integer preferenceDataUsage;
